3f9ec1607a22985078c7d9534935a117dd379ddc
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-29 16:52:56 -0600

    added an id for profile

---
+++ Profile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Profile
+/// </summary>
+public class Profile
+{
+	public Profile()
+	{
+		//Id to identify profile
+		public int ID { get; set; }
+
+	}
+}
