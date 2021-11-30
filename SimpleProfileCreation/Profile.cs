92831f42e21e70a35ad77b25a6a0dac2a563ce4d
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-29 18:46:59 -0600

    changed structure of project

---
+++ SimpleProfileCreation/Profile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleProfileCreation
+{
+    public class Profile
+    {
+        // id for profile identification
+        public int ID { get; set; }
+
+        // username for user
+        public string Username {get; set;}
+
+        // password for user
+        public string Password { get; set; }
+    }
+}
