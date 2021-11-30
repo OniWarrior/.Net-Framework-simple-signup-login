bfb4fdb00e33592489d5e70c0a2ba69db4c7793d
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-29 18:55:23 -0600

    Added AddProfile and GetProfile members to interface

---
+++ SimpleProfileCreation/IProfileDatabase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SimpleProfileCreation
+{
+	public interface IProfileDatabase
+    {
+        Profile AddProfile(Profile profile);
+        Profile GetProfile(int id);
+    }
+}
+	
