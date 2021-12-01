e321101352e64c9131e0c423953f63f2efa87aff
Stephen Aranda <aranda.stephen88@gmail.com>
2021-12-01 11:16:58 -0600

    Implementing IProfileDatabase

---
+++ SimpleProfileCreation.Stores/ProfileDatabase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleProfileCreation.Stores
+{
+    public abstract class ProfileDatabase : IProfileDatabase
+    {
+        public Profile AddProfile(Profile profile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProfile(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Profile GetProfile(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Profile> GetProfiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Profile UpdateProfile(Profile profile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SimpleProfileCreation/IProfileDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleProfileCreation
+{
+    public interface IProfileDatabase
+    {
+        Profile AddProfile(Profile profile);
+        Profile GetProfile(int id);
+        Profile UpdateProfile(Profile profile);
+        IEnumerable<Profile> GetProfiles();
+        void DeleteProfile(int id);
+
+    }
+}
