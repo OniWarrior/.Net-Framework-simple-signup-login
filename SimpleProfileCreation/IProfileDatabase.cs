37321590169b1d7bc61a007081b83aef6f2c336a
Stephen Aranda <aranda.stephen88@gmail.com>
2021-12-01 06:41:46 -0600

    Added imports, GetProfile, and GetAllProfiles members

--- SimpleProfileCreation/IProfileDatabase.cs
+++ SimpleProfileCreation/IProfileDatabase.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SimpleProfileCreation
 {
@@ -6,6 +10,8 @@ namespace SimpleProfileCreation
     {
         Profile AddProfile(Profile profile);
         Profile GetProfile(int id);
+        IEnumerable<Profile> GetAllProfiles();
+
     }
 }
 	
